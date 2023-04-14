bae449ffddce1d6541b2e2a09093fe1a9412d0a1
Zach Norman <zachnorman02@gmail.com>
2023-04-13 21:28:09 -0400

    Navigation stuff

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from './HomeScreen';
-import Tutorial from './Tutorial';
+import HomeScreen from "./HomeScreen";
+import Tutorial from "./Tutorial";
+import { TransitionSpecs } from "@react-navigation/stack";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,8 +12,14 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Tutorial" component={Tutorial} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          name="Tutorial"
+          component={Tutorial}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -20,13 +28,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   buttons: {
     width: "100%",
     flexDirection: "row",
     justifyContent: "space-evenly",
-  }
+  },
 });
